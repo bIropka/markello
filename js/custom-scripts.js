fc44431bfb68c2cac3dc0ca068108dcadf2494e0
Iegor Stetsiura <biropka@gmail.com>
2016-08-13 00:35:50 +0300

    v 0.3

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -4,21 +4,61 @@ $(document).ready(function () {
      ******* init scripts *********
      ******************************/
     var sliderEmployee = $('.slider-employees');
+    var sliderDeveloping = $('.slider-projects-developing');
+    var sliderSeo = $('.slider-projects-seo');
 
     /******************************
      *********** sliders **********
      ******************************/
-
     sliderEmployee.slick({
         arrows: false,
         dots: true
+    });
+    sliderEmployee.on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+        $('.employees-photos li').eq(currentSlide).removeClass('active');
+        $('.employees-photos li').eq(nextSlide).addClass('active');
+    });
+    $('.employees-photos li').click(function() {
+        $('.employees-photos .active').removeClass('active');
+        $(this).addClass('active');
+        sliderEmployee.slick('slickGoTo', $(this).index());
+    });
+
+    sliderDeveloping.slick({
+        slidesToShow: 3,
+        slidesTpScroll: 1,
+        arrows: false
+    });
+
+    sliderSeo.slick({
+        slidesToShow: 3,
+        slidesTpScroll: 1,
+        arrows: false
+    });
+
+    $('.projects-control div').click(function() {
+
+        if (!$(this).hasClass('active')) {
+            var currentIndex = $(this).index();
+            $('.projects-control .active').removeClass('active');
+            $(this).addClass('active');
+            $('.slider-projects ul .active').removeClass('active');
+            $('.slider-projects ul li').eq(currentIndex).addClass('active');
+        }
 
     });
-    sliderEmployee.on('init', function(slick) {
-        //var currentIndex = $('.slider-employees').find('.slick-current').index();
-        console.log(0);
-        $('.employees-photos').eq(currentIndex).addClass('active');
-    })
+
+    $('.slider-control-left').click(function() {
+
+        $('.slider-projects ul .active div').slick('slickPrev');
+
+    });
+
+    $('.slider-control-right').click(function() {
+
+        $('.slider-projects ul .active div').slick('slickNext');
+
+    });
 
 });
 
